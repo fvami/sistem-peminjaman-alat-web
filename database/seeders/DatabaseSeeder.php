e53b2cde1f35e668a1d31372759378252362432c
Fahmi Dwitama <fahmidwitamaja@gmail.com>
2026-02-13 21:42:46 +0800

    91%

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,5 +33,14 @@ class DatabaseSeeder extends Seeder
             ['name' => 'Operator', 'created_at' => now(), 'updated_at' => now()],
             ['name' => 'Administrator', 'created_at' => now(), 'updated_at' => now()],
         ]);
+
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'administrator@gmail.com',
+            'password' => Hash::make('maniac#0'),
+            'role_id' => 2, 
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
